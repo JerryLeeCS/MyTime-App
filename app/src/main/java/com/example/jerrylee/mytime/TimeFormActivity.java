e757876710f79de0cc44e60bb79a8620044d84ce
Jerry Lee <JerryDeKo@gmail.com>
2017-01-16 12:08:27 -0800

    1/16/2017

---
+++ app/src/main/java/com/example/jerrylee/mytime/TimeFormActivity.java
@@ -0,0 +1,13 @@
+package com.example.jerrylee.mytime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TimeFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_form);
+    }
+}
